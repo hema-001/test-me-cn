74a714a88499558716fb18e67c787288acf832ab
hema-001 <hema-001@hotmail.com>
2023-03-01 18:23:36 +0800

    added a feature to specify HSK level

--- README.md
+++ README.md
@@ -22,27 +22,30 @@ Each line should have only one Chinese character and its corresponding pinyin an
 To run the quiz, open a command prompt and navigate to the directory where the files are located.
 Run the following command: 
 ```
-python char_quiz.py <number of characters> 
+python char_quiz.py <HSK level: e.g. 1|2|3|.., or '0' for all chars in file> <number of characters> 
 ```
-where `<number of characters>` is the number of characters you wish to be tested on. For example, 
+Where `<HSK level>` specifies the words to be tested with to be within a given HSK-level. For example, HSK-1 has 150 words, HSK-2 has 300 words and so on. 
+And `<number of characters>` is the number of characters you wish to be tested with. For example, 
 ```
-python char_quiz.py 10 
+python char_quiz.py 1 10 
 ```
-will test you on 10 random characters from the csv file.  
+will test you on 10 random characters from HSK level 1 (HSK-1) fetched from the csv file.  
 Follow the prompts to complete the quiz.  
 At the end of the quiz, the app will show you how many characters you got right out of the total characters in the file, the time it took to complete the quiz, and the characters that you got wrong.
 
 ## Change log
+### 3/1/2023
+- Added a feature to allow users to specify the HSK level of the characters to be tested with
 ### 2/26/2023
-Added a feature to allow users to limit the number of characters to be tested with.
+- Added a feature to allow users to limit the number of characters to be tested with.
 ### 2/12/2023
-Optimized the script, now using dictionaries instead of lists.
+- Optimized the script, now using dictionaries instead of lists.
 ### 2/2/2023
-Added a feature to show the time it took to complete the quiz.
+- Added a feature to show the time it took to complete the quiz.
 ### 2/1/2023
-Added a feature to show the wrong answers at the end of the execution.
+- Added a feature to show the wrong answers at the end of the execution.
 ### 1/29/2023
-Added English translation of the words.
+- Added English translation of the words.
 ### 1/15/2023
-Added correct answer feature; you should see the correct pinyin of the character after your answer.
-Added one more message at the end of the execution to show how many characters you got right out of the total characters in the file.
+- Added correct answer feature; you should see the correct pinyin of the character after your answer.
+- Added one more message at the end of the execution to show how many characters you got right out of the total characters in the file.
--- get_random_char.py
+++ get_random_char.py
@@ -18,41 +18,78 @@ and so on.
 
 
 
-Newly added on 1/15/2023 by Ibrahim M. I. Ismail:
+Added on 1/15/2023 by Ibrahim M. I. Ismail:
 - added correct answer feature; you should see the correct
   pinyin of the character after your answer
 - added one more message at the end of the execution to 
   to show how many characters you got right out of the 
   total characters in file.
 
-Newly added on 1/29/2023 by Ibrahim M. I. Ismail:
+Added on 1/29/2023 by Ibrahim M. I. Ismail:
 - added english translation of the words
 
-Newly added on 1/29/2023 by Ibrahim M. I. Ismail:
+Added on 1/29/2023 by Ibrahim M. I. Ismail:
 - added english translation of the words
 
-Newly added on 2/1/2023 by Ibrahim M. I. Ismail:
+Added on 2/1/2023 by Ibrahim M. I. Ismail:
 - added a feature to show the wrong answers at the end of the execution
 
-Newly added on 2/2/2023 by Ibrahim M. I. Ismail:
+Added on 2/2/2023 by Ibrahim M. I. Ismail:
 - added a feature to show the time it took to complete the quiz
 
-Newly added on 2/12/2023 by Ibrahim M. I. Ismail:
+Added on 2/12/2023 by Ibrahim M. I. Ismail:
 - optimized the script, now using dictionries instead of lists
 
-Newly added on 2/26/2023 by Ibrahim M. I. Ismail:
+Added on 2/26/2023 by Ibrahim M. I. Ismail:
 - added a feature to allow users to limit the number of characters to be tested with
+
+Added on 3/1/2023 by Ibrahim M. I. Ismail:
+- added a feature to allow users to specify the HSK level of the characters to be tested with
 """
 import csv
 import random
 import time
 import sys
 
-def load_csv_file(filename):
+def get_level_words_count(level):
+    """Returns the number of words for each level according to the HSK standards."""
+    if level == 0:
+        return 0
+    elif level == 1:
+        return 150
+    elif level == 2:
+        return 300
+    elif level == 3:
+        return 600
+    elif level == 4:
+        return 1200
+    elif level == 5:
+        return 2500
+    elif level == 6:
+        return 5000
+    
+def get_file_length(filename):
+    """Returns the number of lines in the specified file."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        return sum(1 for row in reader)
+        
+
+def load_csv_file(filename, level):
     """Loads the CSV file and returns a list of characters, pinyin, and English meanings."""
     with open(filename, newline='', encoding='utf-8') as f:
         reader = csv.reader(f)
-        return [(row[0], row[1], row[2]) for row in reader]
+        file_length = get_file_length(filename)
+        words_level = get_level_words_count(level)
+        if words_level > file_length:
+            print(f"Error: The file {filename} has only {file_length} words which is not enough for HSK-{level}.")
+            exit()
+
+        # returns all words in file if level is 0
+        if words_level == 0:
+            return [(row[0], row[1], row[2]) for row in reader]
+        else: # returns words up to the specified level
+            return [(row[0], row[1], row[2]) for i, row in enumerate(reader, start=1) if i <= get_level_words_count(level)]
 
 def quiz(characters, num_chars):
     """Runs a quiz using the specified list of characters."""
@@ -60,10 +97,10 @@ def quiz(characters, num_chars):
     wrong_answers = []
     total_chars = num_chars
     current_char = 0
+    start_time = time.time()
     # initialize the timer
 
     for char, pinyin, eng in characters[:num_chars]:
-        start_time = time.time()
         input_str = input(f"\nHit enter to get a character, or type 'quit' to stop.\n")
         if input_str == 'quit':
             break
@@ -75,10 +112,10 @@ def quiz(characters, num_chars):
         print(f"\n{current_char}/{total_chars}")
         input(f"Hit enter to get the answer.")
         print(f"\nThe right answer is: {pinyin}, in English: {eng}")
-        answer = input(f"\nDid you get it right?, 'Yes' or 'No':")
+        answer = input(f"\nDid you get it right?, 'Y' for yes or 'N' for no:")
 
-        if answer.lower() not in {"yes", "no"}:
-            print("Invalid input. Please enter 'Yes' or 'No'.")
+        if answer.lower() not in {"y", "n", "yes", "no"}:
+            print("Invalid input. Please enter 'Y','Yes' or 'No','N' (case insensitive).")
             continue
 
         if answer.lower() == 'yes':
@@ -91,7 +128,7 @@ def quiz(characters, num_chars):
 
     final_score = score / num_chars
     # print the final time in minutes and seconds
-    print(f"\nYou took {int(total_time / 60)} minutes and {float(total_time % 60)} seconds to complete the quiz.")
+    print(f"\nYou took {int(total_time / 60)} minutes and {int(total_time % 60)} seconds to complete the quiz.")
     print(f"\nYour final score is {int(final_score * 100)}%")
     print(f"You got {score} characters correct out of {num_chars}")
 
@@ -103,12 +140,12 @@ def quiz(characters, num_chars):
 if __name__ == '__main__':
     # get run arguments
     args = sys.argv[1:]
-    if len(args) != 1:
-        print("Usage: python char_quiz.py <number of chars>")
+    if len(args) != 2:
+        print("Usage: python char_quiz.py <HSK level: e.g. 1|2|3|.., or '0' for all chars in file> <number of characters>")
         exit()
-    print(f"Loading {args[0]} characters...")
+    print(f"Loading {args[1]} characters from HSK-{args[0]}...")
     try:
-        characters = load_csv_file("characters.txt")
+        characters = load_csv_file("characters.txt", int(args[0]))
     except FileNotFoundError:
         print("Error: File not found")
         exit()
@@ -117,4 +154,4 @@ if __name__ == '__main__':
         exit()
 
     random.shuffle(characters)
-    quiz(characters, int(args[0]))
+    quiz(characters, int(args[1]))
